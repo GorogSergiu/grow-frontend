e206510d3e9ac5a6cbef40f87370c636c72d2603
Sergiu Gorog <sergiugorog@yahoo.com>
2026-03-14 19:50:12 +0200

    Removed sidebar

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import { Sidebar } from "@/components/app/Sidebar";
 import { AppNavbar } from "@/components/app/AppNavbar";
 
 export default function AppLayout() {
@@ -8,7 +7,6 @@ export default function AppLayout() {
       <AppNavbar />
 
       <div className="mx-auto flex max-w-6xl gap-6 px-4 py-6">
-        <Sidebar />
         <div className="min-w-0 flex-1">
           <Outlet />
         </div>
